1012b8ac0851c01b3f5299a9d6f9376854a028dd
Khubaib Ashraf <khubaibali569@gmail.com>
2026-07-22 05:44:09 -0700

    Add multiplication table function in table.py

---
+++ day2/table.py
@@ -0,0 +1,7 @@
+def multiplication_table():
+    num = int(input("Enter a number: "))
+    print(f"\nMultiplication Table for {num}:")
+    for i in range(1, 11):
+        print(f"{num} x {i} = {num * i}")
+
+multiplication_table()

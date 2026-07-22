10f53ae161bf7d792fbda74d709d64f321bcf128
Khubaib Ashraf <khubaibali569@gmail.com>
2026-07-21 23:37:54 -0700

    Add user information input program

---
+++ day1/info.py
@@ -0,0 +1,15 @@
+#task2.1
+# User Information Input Program
+print("User Information Input Program")
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+college = input("Enter your college name: ")
+course = input("Enter your course/branch: ")
+interest = input("Enter your area of interest: ")
+#task2.2
+print("\n------ User Information ------")
+print("Name:", name)
+print("Age:", age)
+print("College:", college)
+print("Course/Branch:", course)
+print("Area of Interest:", interest)

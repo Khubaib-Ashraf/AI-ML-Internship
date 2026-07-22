a9b97372ae9787b1126405bf2e84ac91afd0c691
Khubaib Ashraf <khubaibali569@gmail.com>
2026-07-21 23:36:42 -0700

    Add welcome message program for Day 1

---
+++ day1/welcome.py
@@ -0,0 +1,12 @@
+#task 1.1
+# Welcome Message Program
+
+print("====================================")
+print(" Welcome to AI/ML Internship Day 1 ")
+#task 1.2
+print("====================================")
+print("Today I am learning Python basics")
+print("1:AI")
+print("2:ML fundamentals")
+print("3:Git")
+print("4:GitHub")

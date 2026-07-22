c3d0f83790515941d5303d45705c26821af1f179
Khubaib Ashraf <khubaibali569@gmail.com>
2026-07-21 23:39:55 -0700

    Add even or odd number checker script

---
+++ day1/odd-even.py
@@ -0,0 +1,9 @@
+#task 4
+# Even or Odd Number Checker
+print(" Even or Odd Number Checker")
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print(number, "is an even number.")
+else:
+    print(number, "is an odd number.")

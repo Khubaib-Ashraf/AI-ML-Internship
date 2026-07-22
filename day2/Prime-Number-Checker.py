a5a1a1df26983035dabbd23e6d51cfd4917b630e
Khubaib Ashraf <khubaibali569@gmail.com>
2026-07-22 05:45:48 -0700

    Add Prime Number Checker function

---
+++ day2/Prime-Number-Checker.py
@@ -0,0 +1,8 @@
+def is_prime(n):
+    if n <= 1: return False
+    for i in range(2, int(n**0.5) + 1): # Optimized: check up to square root
+        if n % i == 0: return False
+    return True
+
+num = int(input("Enter a number: "))
+print(f"{num} is {'Prime' if is_prime(num) else 'Not Prime'}.")
